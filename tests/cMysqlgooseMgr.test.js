1290a84410ce01c20c65464be3768600bdb89f95
aponica-release <github@aponica.com>
2022-09-08 11:40:14 -0400

    test passing filenames and using fpConnect().

--- tests/cMysqlgooseMgr.test.js
+++ tests/cMysqlgooseMgr.test.js
@@ -7,8 +7,6 @@
 //  Unit tests for goosemgr-mysqlgoose-js.
 //-----------------------------------------------------------------------------
 
-const kFs = require( 'fs' );
-
 const kcMysqlgooseMgr = require( '../index.js' );
 
 const zConfigPath = './node_modules/@aponica/mysqlgoose-js/tests-config/';
@@ -19,7 +17,7 @@ test( 'InvalidConnection', fDone => {
 
   const iGooseMgr = new kcMysqlgooseMgr( {} );
 
-  iGooseMgr.fiConnect( {} ).then(
+  iGooseMgr.fpConnect( {} ).then(
     iConn => {
       expect( iConn ).toBe( 'to never happen' );
       fDone();
@@ -36,40 +34,37 @@ test( 'InvalidConnection', fDone => {
 
 test( 'EverythingElse', fDone => {
 
-  const iGooseMgr = new kcMysqlgooseMgr(
-    JSON.parse( kFs.readFileSync( zConfigPath + 'definitions.json' ) ) );
+  const iGooseMgr = new kcMysqlgooseMgr(zConfigPath + 'definitions.json' );
 
-  iGooseMgr.fiConnect(
-    JSON.parse( kFs.readFileSync( zConfigPath + 'config_mysql.json' ) ) ).
-      then( // connect
-        () => { // success
+  iGooseMgr.fpConnect( zConfigPath + 'config_mysql.json' ).then( // connect
+    () => { // success
 
-          iGooseMgr.fiModel( 'customer' ).findById( 1 ).then( // find
+      iGooseMgr.fiModel( 'customer' ).findById( 1 ).then( // find
 
-            hCustomer => { // found
-              expect( hCustomer ).toBeInstanceOf( Object );
-              expect( hCustomer ).toHaveProperty( 'nId' );
-              expect( hCustomer.nId ).toEqual( 1 );
-              iGooseMgr.fiGoose().disconnect();
-              fDone();
-              }, // found
+        hCustomer => { // found
+          expect( hCustomer ).toBeInstanceOf( Object );
+          expect( hCustomer ).toHaveProperty( 'nId' );
+          expect( hCustomer.nId ).toEqual( 1 );
+          iGooseMgr.fiGoose().disconnect();
+          fDone();
+          }, // found
 
-            iErr => { // not found
-              expect( iErr.message ).toBe( 'to never happen' );
-              iGooseMgr.fiGoose().disconnect();
-              fDone();
-              }
-
-            ); // then() // find
-
-          }, // success
-
-        iErr => { // fail
+        iErr => { // not found
           expect( iErr.message ).toBe( 'to never happen' );
+          iGooseMgr.fiGoose().disconnect();
           fDone();
           }
 
-        ); // then() // connect
+        ); // then() // find
+
+      }, // success
+
+    iErr => { // fail
+      expect( iErr.message ).toBe( 'to never happen' );
+      fDone();
+      }
+
+    ); // then() // connect
 
   } ); // test(EverythingElse)
 
